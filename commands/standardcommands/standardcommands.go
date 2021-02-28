71d9cf451991fb7e1c0be1e60bcbae17aa772278
Jordan Johnston <jordanjohnston97@outlook.com>
2021-02-28 16:21:13 +0000

    - add todo

--- commands/standardcommands/standardcommands.go
+++ commands/standardcommands/standardcommands.go
@@ -11,6 +11,7 @@ const Prefix string = "ayame, "
 
 type commandHandler func(session *discordgo.Session, message string) string
 
+// todo: add other commands to bot
 var commandlers = map[string]commandHandler{
 	"setActivity": setActivity,
 }
@@ -34,55 +35,3 @@ func TryHandleStandardCommand(session *discordgo.Session, message *discordgo.Mes
 func setActivity(session *discordgo.Session, message string) string {
 	return "Successfully updated activity!"
 }
-
-/*
-	Todo: need to figure out a more elegant solution for this
-*/
-// func basicCommandHandler(session *discordgo.Session, message *discordgo.MessageCreate) string {
-// 	msgContent := message.Content[len(Prefix):]
-// 	response := ""
-
-// 	if strings.Contains(msgContent, "setActivity") {
-// 		content := msgContent[len("setActivity "):]
-// 		activityType := determineActivity(content)
-// 		status := content[len(activityType)+1:]
-// 		err := setActivity(session, activityType, status)
-// 		handleGenericError(err)
-// 		response = "Successfully updated status!"
-// 	}
-
-// 	return response
-// }
-
-// func determineActivity(content string) string {
-// 	activity := ""
-
-// 	if strings.Contains(content, "playing") {
-// 		activity = "playing"
-// 	} else if strings.Contains(content, "listening") {
-// 		activity = "listening"
-// 	} else if strings.Contains(content, "idle") {
-// 		activity = "idle"
-// 	}
-
-// 	return activity
-// }
-
-// func setActivity(session *discordgo.Session, activityType string, activityMsg string) error {
-// 	idle := 0
-// 	var err error
-
-// 	lcActivityType := strings.ToLower(activityType)
-
-// 	switch lcActivityType {
-// 	case "idle":
-// 		idle = 1
-// 		err = session.UpdateGameStatus(idle, activityMsg)
-// 	case "playing":
-// 		err = session.UpdateGameStatus(idle, activityMsg)
-// 	case "listening":
-// 		err = session.UpdateListeningStatus(activityMsg)
-// 	}
-
-// 	return err
-// }
